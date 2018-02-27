42ebd7290b61bf4a33979ad6a85cb9c1b8869e71
jlweng <22760668+jlweng@users.noreply.github.com>
2018-02-26 22:51:18 -0500

    change background patterns and lengthen transition times

--- js/access.js
+++ js/access.js
@@ -3,6 +3,10 @@ function validatePassword() {
 	if(password == 'm3ow') {
 		window.location.href = "home";
 	}
+	else {
+		document.getElementById("password").className += " animated shake";
+		setTimeout(() => { document.getElementById("password").className = "item"; }, 500);
+	}
 }
 
 function enterHandler(event) {
